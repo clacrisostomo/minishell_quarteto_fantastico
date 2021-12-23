a1e7d64d251abaf52be94bf3e9af8c930d2a2074
cfico-vi <cfico-vi@student.42sp.org.br>
2021-12-23 13:02:21 -0300

    fix: execve with absolut path checker

--- source/execute/execve.c
+++ source/execute/execve.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execve.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmoreira <mmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: cfico-vi <cfico-vi@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 14:11:32 by nbarreir          #+#    #+#             */
-/*   Updated: 2021/12/14 00:46:21 by mmoreira         ###   ########.fr       */
+/*   Updated: 2021/12/23 12:57:54 by cfico-vi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,28 @@ static int	is_executable(char *new_path)
 	return (0);
 }
 
+int		has_second_bar(char *cmd)
+{
+	int	i;
+	int	ctn_slash;
+
+	i = 0;
+	ctn_slash = 0;
+	if(cmd[0] == '.' && cmd[1] == '/')
+		return (TRUE);
+	while (cmd[i])
+	{
+		if (cmd[i] == '/')
+		{
+			ctn_slash++;
+			if (ctn_slash >= 2)
+				return (TRUE);
+		}
+		i++;
+	}
+	return (FALSE);
+}
+
 char	**create_command_for_exec(char **cmd, char **paths)
 {
 	char	*new_path;
@@ -32,10 +54,11 @@ char	**create_command_for_exec(char **cmd, char **paths)
 
 	c = 0;
 	new_cmd = (char **)malloc(sizeof(char **));
-	//ret = ft_strcmp(cmd[0], "./");
+	//ret = ft_strcmp(cmd[0], "./")
 	////printf("RET>>>%i", ret);
-	if (is_executable(cmd[0]) && (cmd[0][0] == '.') && (cmd[0][1] == '/'))
+	if (is_executable(cmd[0]) && has_second_bar(cmd[0]))
 	{
+		//printf("FILIPE RET\n");
 		////printf(" TRINTA E SEIS CMD:%s, RET= %d\n", cmd[0], ret);
 		////printf(" TRINTA E SEIS CMD:%s\n", cmd[0]);
 		new_cmd[0] = ft_strdup(cmd[0]);
@@ -79,9 +102,15 @@ void	do_exec(char **cmd, char **n_env)
 	char		**paths;
 	pid_t		pid;
 	char		**new_cmd;
-
+	//int 		i;
 
 	paths = get_paths();
+/* 	i = 0;
+	while (paths[i])
+	{
+		printf("i = %d\t path = |%s|\n", i, paths[i]);
+		i++;
+	} */
 	new_cmd = NULL;
 	pid = fork();
 	if (pid == -1)
